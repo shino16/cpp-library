bfb66d612b39746f342bdf8755795cf0bac9e1e5
shino16 <masatosinokawa@gmail.com>
2021-11-19 01:05:22 +0000

    .

---
+++ test/DSL_1_A.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A
+
+#include "prelude.hpp"
+#include "ds/uf.hpp"
+
+int n, q, c, x, y;
+int main() {
+   scanf("%d%d", &n, &q);
+   UnionFind uf(n);
+   while (q--) {
+      scanf("%d%d%d", &c, &x, &y);
+      if (c == 0) uf.unite(x, y);
+      else printf("%d\n", (int)uf.same(x, y));
+   }
+}
