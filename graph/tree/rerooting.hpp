30e3a6ce40aad003cef40fe968e2df6f89397d14
shino16 <masatosinokawa@gmail.com>
2022-03-30 21:02:20 +0900

    .

---
+++ graph/tree/rerooting.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "graph.hpp"
+#include "graph/tree/dfs.hpp"
+
+struct identity {
+  template <class T>
+  auto operator()(T&& x) const {
+    return forward<T>(x);
+  }
+};
+
+template <class G, class T, class Add, class Remove, class Inv = identity>
+vector<T> rerooting(const G& graph, T unit = T(), Add&& add = Add(),
+                    Remove remove = Remove(), Inv e_inv = Inv()) {
+  graph_trait<G> g(graph);
+  vector<T> now(g.size(), unit), res(g.size(), unit);
+  dfs(graph, 0, [&](auto&& e, int p) { add(now[p], e, now[e.to()]; });
+  res[0] = now[0];
+  dfs(graph, 0, [&](auto&& e, int p) {
+    remove(now[p], e, now[e.to()]);
+    add(now[e.to()],)
+  });
+}
