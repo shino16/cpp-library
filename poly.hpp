f4fed402d5fe1c411a8880b6b4580db460f60adc
shino16 <masatosinokawa@gmail.com>
2022-07-19 22:57:40 +0900

    .

--- poly.hpp
+++ poly.hpp
@@ -1,105 +1,79 @@
-// #pragma once
-// #include "prelude.hpp"
+#pragma once
+#include "atcoder/convolution.hpp"
+#include "prelude.hpp"
 
-// template <class T>
-// class polynomial {
-//  private:
-//   using self = polynomial<T>;
-//   vector<T> coef;
+template <class T>
+vector<T> conv(vector<T> a, vector<T> b, size_t deg = -1) {
+  if (~deg) a.resize(min(a.size(), deg)), b.resize(min(b.size(), deg));
+  auto f = convolution(a, b);
+  if (~deg) f.resize(deg);
+  return f;
+}
 
-//  public:
-//   template <class... Args>
-//   polynomial(Args&&... args): coef(forward<Args>(args)...) {}
+template <class T>
+vector<T> diff(vector<T> a) {
+  rep(i, a.size() - 1) a[i] = a[i + 1] * (i + 1);
+  a.pop_back();
+  return a;
+}
 
-//   T& operator[](size_t i) { return coef[i]; }
-//   const T& operator[](size_t i) const { return coef[i]; }
-//   size_t size() const { return coef.size(); }
+template <class T>
+vector<T> integr(vector<T> a) {
+  a.push_back(0);
+  repr(i, a.size() - 1) a[i + 1] = a[i] / (i + 1);
+  a[0] = 0;
+  return a;
+}
 
-//   self operator+(const self& that) && {
-//     coef.resize(max(coef.size(), that.size()));
-//     rep(i, b.size()) coef[i] += that[i];
-//     return *this;
-//   }
-//   self& operator+=(const self& that) {
-//     return *this = (*this + that);
-//   }
+template <class T>
+vector<T> inv(vector<T> f, size_t deg) {
+  assert(f[0] != 0);
+  vector<T> g = {1 / f[0]};
+  for (int m = 1; m < deg; m *= 2) {
+    auto h = conv(f, g, m * 2);
+    fill(h.begin(), h.begin() + m, 0);
+    auto hg = conv(h, g, m * 2);
+    rep(i, m * 2) hg[i] = -hg[i];
+    g.insert(g.end(), hg.begin() + m, hg.end());
+  }
+  g.resize(deg);
+  return g;
+}
 
-//   self sub(const self& b) && {
-//     a.resize(max(a.size(), b.size()));
-//     rep(i, b.size()) a[i] -= b[i];
-//     return a;
-//   }
+template <class T>
+vector<T> div(vector<T> a, vector<T> b, size_t deg) {
+  return conv(a, inv(b, deg));
+}
 
-//   vector<mint> mul(mint r) {
-//     rep(i, a.size()) a[i] *= r;
-//     return a;
-//   }
+template <class T>
+vector<T> log(vector<T> a, size_t deg) {
+  assert(a[0] == 1);
+  return integr(div(diff(a), a2, deg - 1), deg - 1);
+}
 
-//   vector<mint> conv(vector<mint> a, vector<mint> b, int deg = -1) {
-//     if (~deg)
-//       a.resize(min<int>(a.size(), deg)),
-//         b.resize(min<int>(b.size(), deg));
-//     auto f = convolution(a, b);
-//     if (~deg) f.resize(deg);
-//     return f;
-//   }
-
-//   vector<mint> diff(vector<mint> a) {
-//     rep(i, a.size() - 1) a[i] = a[i + 1] * (i + 1);
-//     a.pop_back();
-//     return a;
-//   }
-
-//   vector<mint> integr(vector<mint> a) {
-//     a.push_back(0);
-//     repr(i, a.size() - 1) a[i + 1] = a[i] / (i + 1);
-//     a[0] = 0;
-//     return a;
-//   }
-
-//   vector<mint> inv(vector<mint> f, int deg) {
-//     assert(f[0] != 0);
-//     vector<mint> g = {f[0].inv()};
-//     for (int m = 1; m < deg; m *= 2) {
-//       auto h = conv(f, g, m * 2);
-//       fill(h.begin(), h.begin() + m, 0);
-//       auto hg = conv(h, g, m * 2);
-//       rep(i, m * 2) hg[i] = -hg[i];
-//       g.insert(g.end(), hg.begin() + m, hg.end());
-//     }
-//     g.resize(deg);
-//     return g;
-//   }
-
-//   vector<mint> log(vector<mint> a, int deg) {
-//     assert(a[0] == 1);
-//     vector<mint> a2 = a;
-//     return integr(conv(diff(move(a)), inv(move(a2), deg - 1), deg - 1));
-//   }
-
-//   vector<mint> exp(vector<mint> h, int deg) {
-//     assert(h[0] == 0);
-//     int n = 1 << atcoder::internal::ceil_pow2(deg);
-//     h.resize(n);
-//     vector<mint> f = {1}, g = {1};
-//     vector<mint> q(h.size() - 1);
-//     rep(i, q.size()) q[i] = h[i + 1] * (i + 1);
-//     for (int m = 1; m < n; m *= 2) {
-//       auto fgg = conv(conv(f, g, m), g, m);
-//       g.resize(m);
-//       rep(i, m) g[i] = g[i] * 2 - fgg[i];
-//       auto fq = conv(f, {q.begin(), q.begin() + m - 1});
-//       rep(i, fq.size()) fq[i] =
-//         -fq[i] + (i < f.size() - 1 ? f[i + 1] * (i + 1) : 0);
-//       auto w = conv(g, fq, m * 2 - 1);
-//       rep(i, m - 1) w[i] += q[i];
-//       w.push_back(0);
-//       repr(i, m * 2 - 1) w[i + 1] = h[i + 1] - w[i] / (i + 1);
-//       w[0] = h[0];
-//       auto fw = conv(f, w, m * 2);
-//       f.resize(m * 2);
-//       rep(i, m * 2) f[i] += fw[i];
-//     }
-//     return f;
-//   }
-// };
+template <class T>
+vector<T> exp(vector<T> h, size_t deg) {
+  assert(h[0] == 0);
+  int n = 1 << atcoder::internal::ceil_pow2(deg);
+  h.resize(n);
+  vector<T> f = {1}, g = {1};
+  vector<T> q(h.size() - 1);
+  rep(i, q.size()) q[i] = h[i + 1] * (i + 1);
+  for (int m = 1; m < n; m *= 2) {
+    auto fgg = conv(conv(f, g, m), g, m);
+    g.resize(m);
+    rep(i, m) g[i] = g[i] * 2 - fgg[i];
+    auto fq = conv(f, {q.begin(), q.begin() + m - 1});
+    rep(i, fq.size()) fq[i] =
+        -fq[i] + (i < f.size() - 1 ? f[i + 1] * (i + 1) : 0);
+    auto w = conv(g, fq, m * 2 - 1);
+    rep(i, m - 1) w[i] += q[i];
+    w.push_back(0);
+    repr(i, m * 2 - 1) w[i + 1] = h[i + 1] - w[i] / (i + 1);
+    w[0] = h[0];
+    auto fw = conv(f, w, m * 2);
+    f.resize(m * 2);
+    rep(i, m * 2) f[i] += fw[i];
+  }
+  return f;
+}
