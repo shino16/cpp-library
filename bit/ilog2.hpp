93d034d21aaaf1a37a287f4300228a4099ae9727
Masato Shinokawa <masatosinokawa@gmail.com>
2023-10-07 20:08:00 +0100

    Add __attribute__((pure)) to supress warning

--- bit/ilog2.hpp
+++ bit/ilog2.hpp
@@ -3,13 +3,13 @@
 #include "bit/clz.hpp"
 
 template <class T>
-int ilog2(T x) { assert(x != 0); return sizeof(T) * 8 - 1 - clz(x); }
+__attribute__((pure)) int ilog2(T x) { assert(x != 0); return sizeof(T) * 8 - 1 - clz(x); }
 
 template <class T>
-int ilog2_ceil(T x) { return x == 0 || x == 1 ? 0 : ilog2(x - 1) + 1; }
+__attribute__((pure)) int ilog2_ceil(T x) { return x == 0 || x == 1 ? 0 : ilog2(x - 1) + 1; }
 
 template <class T>
-T bit_floor(T x) { return T(1) << ilog2(x); }
+__attribute__((pure)) T bit_floor(T x) { return T(1) << ilog2(x); }
 
 template <class T>
-T bit_ceil(T x) { return T(1) << ilog2_ceil(x); }
+__attribute__((pure)) T bit_ceil(T x) { return T(1) << ilog2_ceil(x); }
