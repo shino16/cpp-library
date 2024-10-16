b69953cbdb1a359c42947991bb6f4712c8bbfe1f
shino16 <shinokawa.m+git@gmail.com>
2024-10-16 16:33:39 +0100

    .

--- arith/sat.hpp
+++ arith/sat.hpp
@@ -6,7 +6,7 @@ auto sat_add(T a, U b) {
   using V = common_type_t<T, U>;
   V res;
   return __builtin_add_overflow((V)a, (V)b, &res)
-             ? (a < 0 ? numeric_limits<T>::min() : numeric_limits<T>::max())
+             ? (a < 0 ? numeric_limits<V>::min() : numeric_limits<V>::max())
              : res;
 }
 template <class T, class U>
@@ -14,7 +14,7 @@ auto sat_sub(T a, U b) {
   using V = common_type_t<T, U>;
   V res;
   return __builtin_sub_overflow((V)a, (V)b, &res)
-             ? (a < 0 ? numeric_limits<T>::min() : numeric_limits<T>::max())
+             ? (a < 0 ? numeric_limits<V>::min() : numeric_limits<V>::max())
              : res;
 }
 template <class T, class U>
@@ -22,7 +22,7 @@ auto sat_mul(T a, U b) {
   using V = common_type_t<T, U>;
   V res;
   return __builtin_mul_overflow((V)a, (V)b, &res)
-             ? ((a < 0) == (b < 0) ? numeric_limits<T>::max()
-                                   : numeric_limits<T>::min())
+             ? ((a < 0) == (b < 0) ? numeric_limits<V>::max()
+                                   : numeric_limits<V>::min())
              : res;
 }
